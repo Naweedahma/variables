272856ce6ad3f3bf2cae9785dff0e5ba4f5cdc0d
Naweedahma <100299521+Naweedahma@users.noreply.github.com>
2022-02-24 11:08:58 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    number += 1
+    basic.showNumber(number)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    Sprite.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    number += -1
+    basic.showNumber(number)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    Sprite.change(LedSpriteProperty.X, 1)
+})
+let Sprite: game.LedSprite = null
+let number = 0
+basic.showIcon(IconNames.Heart)
+number = 7
+Sprite = game.createSprite(2, 2)
+basic.forever(function () {
+	
+})
